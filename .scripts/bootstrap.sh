11e6272556878e8d476e4cb1e984a2b4bd500988
Sandesh Ghimire <sandeshghimire@gmail.com>
2018-01-31 14:09:44 -0800

    Adding stuff

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -3,11 +3,8 @@
 echo "Setting up system,this will take a long time"
 echo "depending on our pc and internet connection"
 
-
 echo "installing dependencies "
 sudo apt-get install autoconf automake libtool libexpat1-dev  libncurses5-dev bison flex patch curl cvs texinfo git bc  build-essential subversion gawk python-dev gperf unzip  pkg-config wget help2man  sed wget cvs subversion git-core coreutils unzip texi2html texinfo libsdl1.2-dev docbook-utils gawk python-pysqlite2 diffstat help2man make gcc build-essential g++ desktop-file-utils chrpath libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff libtool xterm gawk wget git-core diffstat unzip texinfo build-essential chrpath socat libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake libtool libglib2.0-dev python-gtk2 bsdmainutils screen
-
-
 setup host name
 hostnamectl set-hostname 'bowers-wilkins-yocto-dev'
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
