ae4b14b8bbd05ab2a2bcf99c6a3935ded933131c
Sandesh J. Ghimire <sandesh@aeva.ai>
2018-02-06 14:52:46 -0800

    Adding uuid to hostname

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -6,7 +6,7 @@ echo "depending on our pc and internet connection"
 echo "installing dependencies "
 sudo apt-get install autoconf automake libtool libexpat1-dev  libncurses5-dev bison flex patch curl cvs texinfo git bc  build-essential subversion gawk python-dev gperf unzip  pkg-config wget help2man  sed wget cvs subversion git-core coreutils unzip texi2html texinfo libsdl1.2-dev docbook-utils gawk python-pysqlite2 diffstat help2man make gcc build-essential g++ desktop-file-utils chrpath libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff libtool xterm gawk wget git-core diffstat unzip texinfo build-essential chrpath socat libsdl1.2-dev xterm make xsltproc docbook-utils fop dblatex xmlto autoconf automake libtool libglib2.0-dev python-gtk2 bsdmainutils screen
 setup host name
-hostnamectl set-hostname 'embedded-linux-build-server'
+
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
@@ -32,3 +32,7 @@ sudo apt-get install openssh-server apt-get install net-tools  net-tools  git  v
 sudo dpkg-reconfigure dash
  git config --global user.name "Sandesh J. Ghimire"
  git config --global user.email "sandesh@aeva.ai"
+
+sudo apt install uuid
+hostname=uuid 
+hostnamectl set-hostname 'embedded-linux-build-server'-$hostname
